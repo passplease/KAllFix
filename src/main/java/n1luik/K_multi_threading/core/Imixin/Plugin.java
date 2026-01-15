9e8e831e61691bf6a85bf56a69c4fa66166a70a5
1luik <8632787+n1luik@user.noreply.gitee.com>
2026-01-15 17:07:54 +0800

    增加对各种功能有必要的[瓦尔基里]valkyrienskies的全面兼容-正在开发 起码能启动，正在做一些别的什么，修复多线程跟[Alchemy Kingdom, nameless trinkets]的兼容问题，尝试修复新版本l2damagetracker跟多线程的兼容问题，继续修复c2me，让瓦尔基里可以2.4启动但是没有兼容，可以使用-DKMT-OpenVanillaServerChunkCache强制打开原版ServerChunkCache修复

--- src/main/java/n1luik/K_multi_threading/core/Imixin/Plugin.java
+++ src/main/java/n1luik/K_multi_threading/core/Imixin/Plugin.java
@@ -34,7 +34,7 @@ public class Plugin implements IMixinConfigPlugin {
         return switch (mixinClassName) {
             case "n1luik.K_multi_threading.core.mixin.minecraftfix.ChunkMapFix2" -> UnsafeEnable.INSTANCE.SafeUnloadChunk;
             case "n1luik.K_multi_threading.core.mixin.impl.MinecraftServerImpl2" -> UnsafeEnable.INSTANCE.IndependencePlayer;
-            case "n1luik.K_multi_threading.core.mixin.minecraftfix.ServerChunkCacheFix2" -> !(isModLoaded("canary") || isModLoaded("radium") || isModLoaded("lithium"));
+            case "n1luik.K_multi_threading.core.mixin.minecraftfix.ServerChunkCacheFix2" -> (!(isModLoaded("canary") || isModLoaded("radium") || isModLoaded("lithium"))) || Boolean.getBoolean("KMT-OpenVanillaServerChunkCache");
             case "n1luik.K_multi_threading.core.mixin.minecraftfix.LegacyRandomSourceFix2" -> !isModLoaded("structureessentials");
             case "n1luik.K_multi_threading.core.mixin.minecraftfix.ServerWatchdogFix1" -> !isModLoaded("fullstackwatchdog");
             default -> true;
